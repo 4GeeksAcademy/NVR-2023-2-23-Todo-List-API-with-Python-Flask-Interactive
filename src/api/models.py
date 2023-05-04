81e8be2668649007ad237432057e25910c91185c
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-05-04 19:01:47 +0000

    GET all todos endpoint created - 04/05/2023

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,14 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(120), unique=True, nullable=False)
+    done = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "label": self.label,
+            "done": self.done,
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Todo
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,14 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/todos', methods=['GET'])
+def get_all_todos():
+  
+    response_body = {
+        "message": "List of all active Toddos:"
+    }
+
+    todos = Todo.query.all()
+    todos_serialzied =  [todo.serialize() for todo in todos]
+    return jsonify({"todos": todos_serialzied}), 200
